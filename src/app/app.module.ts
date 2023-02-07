6e081dc73844fd48c29f6f370d95809e75472ccd
Vladimir Stojkovic <dovlasno1@gmail.com>
2023-02-07 18:52:25 +0100

    chore: firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-scifiz-app-43749',
+    appId: '1:895417809891:web:897f2f1a0c9cd0f4aa7ed6',
+    storageBucket: 'angular-scifiz-app-43749.appspot.com',
+    apiKey: 'AIzaSyBnEUkjf5zClelmgJnEmuOefjEx4REmlJE',
+    authDomain: 'angular-scifiz-app-43749.firebaseapp.com',
+    messagingSenderId: '895417809891',
+  },
   production: true
 };
