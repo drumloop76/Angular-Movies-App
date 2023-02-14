7c8506bb8ff1a3b4a8c01e1c72132e9bacb72be8
Vladimir Stojkovic <dovlasno1@gmail.com>
2023-02-14 12:16:38 +0100

    feat: add footer, theme colors

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { navMediaItems } from 'src/app/shared/helpers/helpers';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
 
+export class FooterComponent {
+  mediaLinks: any[] = [];
+
+  constructor() {
+    this.mediaLinks = navMediaItems;
+  }
 }
